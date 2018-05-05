79f0afbacd776677f32e358e0cc6cf2a7db36dbe
Karl Semich <0xloem@gmail.com>
2018-05-05 07:02:01 -0400

    standard style

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var hyperdb = require('hyperdb')
 var thunky = require('thunky')
-var path = require('path')
 var events = require('events')
 var inherits = require('inherits')
 var sodium = require('sodium-universal')
@@ -21,9 +20,9 @@ function HyperStream (storage, key, opts) {
   if (!opts) opts = {}
   if (!opts.contentFeed) opts.contentFeed = true
 
-  this.db = new hyperdb(storage, key, opts)
+  this.db = hyperdb(storage, key, opts)
   this.ready = thunky(this._ready.bind(this))
-  
+
   this.ready()
 }
 
@@ -32,7 +31,7 @@ inherits(HyperStream, events.EventEmitter)
 HyperStream.prototype._ready = function (cb) {
   var self = this
   this.db.ready(function (err) {
-    if (err) return cb(err);
+    if (err) return cb(err)
 
     self.id = self.db.local.discoveryKey.toString('base64')
     self._feed = self.db.localContent
@@ -41,59 +40,56 @@ HyperStream.prototype._ready = function (cb) {
   })
 }
 
-HyperStream.prototype.write = function(data, cb) {
-  var self = this;
-  var checkpoint;
+HyperStream.prototype.write = function (data, cb) {
+  var self = this
 
-  this._feed.append(data, function(err) {
+  this._feed.append(data, function (err) {
     // if an error is thrown, the batcher will crash and never write again
     try {
       if (err) return cb(err)
 
       self._writeCheckpoint(cb)
-    } catch(e) {
-      self.emit("error", e)
+    } catch (e) {
+      self.emit('error', e)
     }
   })
 }
 
-HyperStream.prototype._writeCheckpoint = function(cb) {
-  var self = this;
+HyperStream.prototype._writeCheckpoint = function (cb) {
+  var self = this
   var checkpoint = {
     length: this._feed.length,
     byteLength: this._feed.byteLength,
     rootHash: null
   }
-    
-  hashRoots(this._feed, checkpoint.length - 1, function(err, hash) {
+
+  hashRoots(this._feed, checkpoint.length - 1, function (err, hash) {
     if (err) return cb(err)
 
     checkpoint.rootsHash = hash
 
-    self.db.put(self.id + "/checkpoint", messages.Checkpoint.encode(checkpoint), done)
+    self.db.put(self.id + '/checkpoint', messages.Checkpoint.encode(checkpoint), done)
   })
 
-  function done(err) {
+  function done (err) {
     try {
       cb(err)
-    } catch(e) {
-      self.emit("error", e) 
+    } catch (e) {
+      self.emit('error', e)
     }
   }
 }
 
-function hashRoots(feed, index, cb)
-{
-  feed.rootHashes(index, function(err, roots) {
-    if (err) return cb(err);
+function hashRoots (feed, index, cb) {
+  feed.rootHashes(index, function (err, roots) {
+    if (err) return cb(err)
 
     var digest = bufferAlloc(32)
-  
-    for (var i = 0; i < roots.length; i++)
-      roots[i] = roots[i].hash
-  
+
+    for (var i = 0; i < roots.length; i++) { roots[i] = roots[i].hash }
+
     sodium.crypto_generichash_batch(digest, roots)
-  
+
     cb(null, digest)
   })
 }
