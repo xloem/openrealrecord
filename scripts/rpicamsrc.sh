48fc513fdcb642bb3af42b3a9219d7e4c3fe1337
Karl Semich <0xloem@gmail.com>
2018-06-09 18:58:51 -0400

    switch from matroska to plain h264 for now; it can be read mid-stream

--- scripts/rpicamsrc.sh
+++ scripts/rpicamsrc.sh
@@ -11,4 +11,8 @@ then
   $ORR --storage ${ROOT}/${NAME} --my-name "rpi $rpiver $rpiser video" --sync
 fi
 
-gst-launch-1.0 --quiet rpicamsrc preview=false annotation-mode=$((0x40c)) annotation-text-size=16 ! video/x-h264,width=1296,height=972,framerate=42/1 ! h264parse ! matroskamux ! fdsink | $ORR --storage ${ROOT}/${NAME} --serve --pipe --incache 1G:8G
+# these particular h.264 streams start with 00 00 00 01 27
+# that byte sequence marks all valid starting offsets; can seek forward to find it
+# i think in general the '2' in '27' may change
+
+gst-launch-1.0 --quiet rpicamsrc preview=false annotation-mode=$((0x40c)) annotation-text-size=16 ! video/x-raw,width=1296,height=972,framerate=42/1 ! omxh264enc ! fdsink | $ORR --storage ${ROOT}/${NAME} --serve --pipe --incache 1G:8G
